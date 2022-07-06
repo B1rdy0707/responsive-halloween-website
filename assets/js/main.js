862f61eff464dab18f3bcb64f77623e5bc48bbb9
b1rdy0707 <754244855@qq.com>
2022-07-06 22:28:18 +0800

    Firstcommit

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,6 @@ if (navToggle) {
     navMenu.classList.add("show-menu");
   });
 }
-
 /*========= MENU HIDDEN ========= */
 // validate if constant exists
 
@@ -24,18 +23,34 @@ if (navClose) {
 
 /*==================== REMOVE MENU MOBILE ====================*/
 
-const navLink = document.querySelectorAll(".nav__link");
-
-function linkAction() {
-  const navMenu = document.getElementById("nav-menu");
-  // When we click on each nav__link, we remove the show-menu class
-  navMenu.classList.remove("show-menu");
-}
-navLink.forEach((n) => n.addEventListener("click", linkAction));
-
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 50) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
 
-/*=============== POPULAR SWIPER ===============*/
+/*=============== HOME SWIPER ===============*/
+
+var homeSwiper = new Swiper(".home-swiper", {
+  spaceBetween: 30,
+  loop: "true",
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+/*=============== NEW SWIPER ===============*/
+let newSwiper = new Swiper(".new-swiper", {
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: "true",
+  spaceBetween: 16,
+});
 
 /*=============== MIXITUP FILTER FEATURED ===============*/
 
@@ -43,6 +58,28 @@ navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== SHOW SCROLL UP ===============*/
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
