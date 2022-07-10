ad5e0726c3072d63f0ef9169fe28a97a133be29f
b1rdy0707 <754244855@qq.com>
2022-07-10 10:53:37 +0800

    ThirdCommit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,40 +1,36 @@
 /*=============== SHOW MENU ===============*/
-
 const navMenu = document.getElementById("nav-menu"),
   navToggle = document.getElementById("nav-toggle"),
   navClose = document.getElementById("nav-close");
 
-/*========= MENU SHOW ========= */
-// validate if constant exists
-
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
 if (navToggle) {
   navToggle.addEventListener("click", () => {
     navMenu.classList.add("show-menu");
   });
 }
-/*========= MENU HIDDEN ========= */
-// validate if constant exists
 
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
 if (navClose) {
   navClose.addEventListener("click", () => {
     navMenu.classList.remove("show-menu");
   });
 }
 
-/*==================== REMOVE MENU MOBILE ====================*/
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
 
-/*=============== CHANGE BACKGROUND HEADER ===============*/
-function scrollHeader() {
-  const header = document.getElementById("header");
-  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-  if (this.scrollY >= 50) header.classList.add("scroll-header");
-  else header.classList.remove("scroll-header");
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
 }
-window.addEventListener("scroll", scrollHeader);
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== HOME SWIPER ===============*/
-
-var homeSwiper = new Swiper(".home-swiper", {
+let homeSwiper = new Swiper(".home-swiper", {
   spaceBetween: 30,
   loop: "true",
 
@@ -44,14 +40,6 @@ var homeSwiper = new Swiper(".home-swiper", {
   },
 });
 
-/*=============== NEW SWIPER ===============*/
-let newSwiper = new Swiper(".new-swiper", {
-  centeredSlides: true,
-  slidesPerView: "auto",
-  loop: "true",
-  spaceBetween: 16,
-});
-
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader() {
   const header = document.getElementById("header");
@@ -61,28 +49,24 @@ function scrollHeader() {
 }
 window.addEventListener("scroll", scrollHeader);
 
-/* Link active featured */
+/*=============== NEW SWIPER ===============*/
+let newSwiper = new Swiper(".new-swiper", {
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: "true",
+  spaceBetween: 16,
+});
 
-/*=============== SHOW SCROLL UP ===============*/
-
-function scrollUp() {
-  const scrollUp = document.getElementById("scroll-up");
-  // When the scroll is higher than 460 viewport height, add the show-scroll class to the a tag with the scroll-top class
-  if (this.scrollY >= 460) scrollUp.classList.add("show-scroll");
-  else scrollUp.classList.remove("show-scroll");
-}
-window.addEventListener("scroll", scrollUp);
-
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll("section[id]");
 
 function scrollActive() {
   const scrollY = window.pageYOffset;
 
   sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight;
-    const sectionTop = current.offsetTop - 50;
-    sectionId = current.getAttribute("id");
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id");
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
       document
@@ -96,6 +80,15 @@ function scrollActive() {
   });
 }
 window.addEventListener("scroll", scrollActive);
+
+/*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 460 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 460) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
